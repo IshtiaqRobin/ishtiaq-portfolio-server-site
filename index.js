01e08fadd65128aa99ed9643c3d000e23d70a444
Ishtiaq <robin.pass36@gmail.com>
2022-12-13 14:20:12 +0600

    backend server code added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const projects = require('./data/projects.json');
+const projectsDetails = require('./data/projectsDetails.json');
+
+// * middleware
+app.use(cors());
+
+app.get('/projects', (req, res) => {
+    res.send(projects);
+});
+
+app.get('/project/:id', (req, res) => {
+    const id = req.params.id;
+    const project = projectsDetails.filter(n => n.projects_id === id);
+    res.send(project);
+});
+
+
+// * test method
+app.get('/', (req, res) => {
+    res.send('portfolio side is running')
+});
+
+app.listen(port, () => console.log(`portfolio side running on ${port}`));
